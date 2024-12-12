e42276de570ebe1576d1d3cd0bd21aa6b57cd744
Jason <161390038+jasoncarcano@users.noreply.github.com>
2024-12-11 23:04:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ document.getElementById("contactForm").addEventListener("submit", function(e) {
     alert("Thank you for your message! We'll get back to you soon.");
 });
 
+const menuItems = document.querySelectorAll(".menu-item");
+
+menuItems.forEach(item => {
+    item.addEventListener("mouseover", () => {
+        item.style.backgroundColor = "#5a8fbc";
+        item.style.color = "white";
+    });
+    item.addEventListener("mouseout", () => {
+        item.style.backgroundColor = "transparent";
+        item.style.color = "black";
+    });
+});
