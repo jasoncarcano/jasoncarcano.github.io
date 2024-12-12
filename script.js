52bdcb8d52c0aeb93b04e36cac0d368561169ddc
Jason <161390038+jasoncarcano@users.noreply.github.com>
2024-12-11 20:54:39 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   const menuToggle = document.getElementById('menu-toggle');
+  const menuCloseButton = document.getElementById('menu-close-button');
   const navMenu = document.querySelector('.nav-menu');
 
-  menuToggle.addEventLister('click', () => {
-    navMenu.classList.toggle('open');
+  menuToggle.addEventListener('click', () => {
+    navMenu.classList.add('open');
+  });
+
+  menuCloseButton.addEventListener('click', () => {
+    navMenu.classList.remove('open');
   });
   
   const form = document.getElementById('contact-form');
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     alert('Thank you for supporting us, we will get back to you as soon as possible');
-    contactForm.reset();
+    Form.reset();
   });
 });
